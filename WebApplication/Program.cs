3adb528f1d22f3a9882b898aa5247c5f2f7b61d0
e-iroom <33768879+e-iroom@users.noreply.github.com>
2021-08-26 16:55:27 +0900

    機能追加

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/// <summary>
+/// addContorols
+/// </summary>
 namespace WebApplication
 {
     public class Program
